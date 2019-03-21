6ff4e8ab88bea72233306273d312768d16bbde04
evangelalessandro <evangel.alessandro@gmail.com>
2019-03-21 08:27:56 +0100

    Aggiungere i file di progetto.

---
+++ FastCodeWriteOre/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FastCodeWriteOre
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variabile di progettazione necessaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Pulire le risorse in uso.
+		/// </summary>
+		/// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Codice generato da Progettazione Windows Form
+
+		/// <summary>
+		/// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+		/// il contenuto del metodo con l'editor di codice.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+			this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+			this.button2 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(27, 239);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(264, 46);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Esporta ore";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.dateTimePicker1.Location = new System.Drawing.Point(53, 29);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker1.TabIndex = 1;
+			this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+			this.dateTimePicker1.Validated += new System.EventHandler(this.dateTimePicker1_Validated);
+			// 
+			// dateTimePicker2
+			// 
+			this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.dateTimePicker2.Location = new System.Drawing.Point(53, 55);
+			this.dateTimePicker2.Name = "dateTimePicker2";
+			this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker2.TabIndex = 2;
+			this.dateTimePicker2.Validated += new System.EventHandler(this.dateTimePicker2_Validated);
+			// 
+			// dateTimePicker3
+			// 
+			this.dateTimePicker3.Enabled = false;
+			this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.dateTimePicker3.Location = new System.Drawing.Point(53, 81);
+			this.dateTimePicker3.Name = "dateTimePicker3";
+			this.dateTimePicker3.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker3.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(15, 129);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(264, 46);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Calcola Differenza orari";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.dateTimePicker1);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.dateTimePicker2);
+			this.groupBox1.Controls.Add(this.dateTimePicker3);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(301, 201);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Calcola differenae orarie";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(337, 306);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker2;
+		private System.Windows.Forms.DateTimePicker dateTimePicker3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.GroupBox groupBox1;
+	}
+}
+
---
+++ FastCodeWriteOre/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FastCodeWriteOre
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public string DiarioCantiere { get; set; }
+		public string FileRaccoltaDati { get; set; }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//DiarioCantiere = @"C:\Users\fastcode13042017\Desktop\OrariCantieri\FASTCODE-EVANGELISTI_ALESSANDRO-Diario_Sede_Cantiere.xlsm";
+			//FileRaccoltaDati = @"C:\Users\fastcode13042017\Desktop\RaccoltaDati x dip Cantieri Evangelisti Alessandro_Dic_2019.xlsx";
+
+			SelectFile();
+		}
+		private void SelectFile()
+		{
+			using (OpenFileDialog res = new OpenFileDialog())
+			{
+				if (DiarioCantiere == null)
+				{
+					res.Title = "Seleziona file diario cantiere";
+					//Filter
+					res.Filter = "File excel|*fastcode*.xlsm;|Tutti i file|*.*";
+
+					res.Multiselect = false;
+					//When the user select the file
+					if (res.ShowDialog() == DialogResult.OK)
+					{
+						DiarioCantiere = res.FileName;
+
+					}
+				}
+				if (FileRaccoltaDati == null)
+				{
+					res.Title = "Seleziona file RaccoltaDati x dip Cantieri";
+					//Filter
+					res.Filter = "File excel|*.xls;*.xlsx|Tutti i file|*.*";
+
+					res.Multiselect = false;
+					//When the user select the file
+					if (res.ShowDialog() == DialogResult.OK)
+					{
+						FileRaccoltaDati = res.FileName;
+
+					}
+				}
+				if (FileRaccoltaDati != null && DiarioCantiere != null)
+				{
+					Elabora();
+				}
+			}
+		 
+		}
+
+		private void Elabora()
+		{
+
+
+			using (var excelRaccoltaDati = new ClosedXML.Excel.XLWorkbook(FileRaccoltaDati))
+			{
+				using (var excelDiario = new ClosedXML.Excel.XLWorkbook(DiarioCantiere))
+				{
+
+					var row = 3;
+
+					var rowDiarioLast = excelDiario.Worksheet(1).Range("B:B").LastCellUsed().Address.RowNumber;
+
+					var list = new List<OreCantiere>();
+					ClosedXML.Excel.IXLWorksheet meseSheet = excelRaccoltaDati.Worksheet(2);
+					while (meseSheet.Cell(row, 1).Value != null)
+					{
+						if (meseSheet.Cell(row, 1).Value.ToString() == "")
+						{
+							break;
+						}
+						var dataCel = DateTime.Parse(meseSheet.Cell(row, 1).Value.ToString()).Date;
+
+						for (int i = 6; i < rowDiarioLast; i++)
+						{
+							var dataDiario = DateTime.Parse(excelDiario.Worksheet(1).Cell(i, 2).Value.ToString()).Date;
+							if (dataDiario == dataCel)
+							{
+								list.Add(new OreCantiere()
+								{
+									Commessa = excelDiario.Worksheet(1).Cell(i, 3).Value.ToString(),
+									Data = DateTime.Parse( dataCel.ToString()).Date,
+									Ore = decimal.Parse(excelDiario.Worksheet(1).Cell(i, 7).Value.ToString()),
+									Cantiere = excelDiario.Worksheet(1).Cell(i, 5).Value.ToString() == "C",
+
+								});
+							}
+						}
+						row++;
+					}
+
+
+					var listOre = list.GroupBy(a => new { a.Commessa, a.Cantiere, a.Data }).Select(a => new { a.Key.Cantiere, a.Key.Commessa, a.Key.Data, SommaOre = a.Sum(b => b.Ore) }).ToList();
+					
+					foreach (var item in listOre)
+					{
+						row = 3;
+						while (meseSheet.Cell(row, 1).Value != null)
+						{
+							if (meseSheet.Cell(row, 1).Value.ToString() == "")
+							{
+								break;
+							}
+							var dataCel = DateTime.Parse(meseSheet.Cell(row, 1).Value.ToString()).Date;
+							if (dataCel==item.Data)
+							{
+								if (meseSheet.Cell(row, 3).IsEmpty())
+								{
+
+									ScriviRiga(meseSheet, row, item.Commessa, item.SommaOre, item.Cantiere);
+									break;
+								}
+
+							}
+							row++;
+						}
+					}
+					excelRaccoltaDati.Save();
+				}
+
+			}
+
+			MessageBox.Show("Operazione conclusa con successo");
+
+
+		}
+		private void ScriviRiga(ClosedXML.Excel.IXLWorksheet meseSheet,int row,string commessa,decimal sommaOre,bool cantiere)
+		{
+			meseSheet.Cell(row, 3).Value = commessa;
+			meseSheet.Cell(row, 5).Value = sommaOre;
+			if (cantiere)
+			{
+				meseSheet.Cell(row, 4).Value = "Cantiere Italia";
+				meseSheet.Cell(row, 12).Value = "x";
+				meseSheet.Cell(row, 13).Value = "x";
+				meseSheet.Cell(row, 14).Value = "x";
+			}
+			else
+			{
+				meseSheet.Cell(row, 4).Value = "Sede System o telework";
+			}
+		}
+		internal class OreCantiere
+		{
+			public DateTime Data { get; set; }
+			public string Commessa { get; set; }
+			public decimal Ore { get; set; }
+			public bool Cantiere { get; set; }
+		}
+
+		private void dateTimePicker2_Validated(object sender, EventArgs e)
+		{
+			CalcolaDiff();
+		}
+
+		private void CalcolaDiff()
+		{
+			dateTimePicker3.Value = new DateTime(dateTimePicker2.Value.Date.Ticks + (dateTimePicker2.Value - dateTimePicker1.Value).Ticks);
+		}
+
+		private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dateTimePicker1_Validated(object sender, EventArgs e)
+		{
+			CalcolaDiff();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			CalcolaDiff();
+		}
+	}
+}
