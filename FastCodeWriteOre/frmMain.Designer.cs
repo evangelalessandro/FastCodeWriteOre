7d29edcf28edfe820afb3b7ae3e2ff2110742be1
evangale <fastcodedevops@outlook.it>
2024-04-17 18:05:20 +0200

    aggiunto tasto per aggiungere mese a date export

--- FastCodeWriteOre/frmMain.Designer.cs
+++ FastCodeWriteOre/frmMain.Designer.cs
@@ -1,170 +1,184 @@
 ﻿namespace FastCodeWriteOre
 {
-	partial class frmMain
-	{
-		/// <summary>
-		/// Variabile di progettazione necessaria.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+  partial class frmMain
+  {
+    /// <summary>
+    /// Variabile di progettazione necessaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Pulire le risorse in uso.
-		/// </summary>
-		/// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-
-		#region Codice generato da Progettazione Windows Form
-
-		/// <summary>
-		/// Metodo necessario per il supporto della finestra di progettazione. Non modificare
-		/// il contenuto del metodo con l'editor di codice.
-		/// </summary>
-		private void InitializeComponent()
-		{
-            this.button1 = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
-            this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dtExportStart = new System.Windows.Forms.DateTimePicker();
-            this.dtExportEnd = new System.Windows.Forms.DateTimePicker();
-            this.chkEstero = new System.Windows.Forms.CheckBox();
-            this.groupBox1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(36, 294);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 57);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Esporta ore";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePicker1.Location = new System.Drawing.Point(71, 36);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
-            this.dateTimePicker1.TabIndex = 1;
-            this.dateTimePicker1.Validated += new System.EventHandler(this.dateTimePicker1_Validated);
-            // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePicker2.Location = new System.Drawing.Point(71, 68);
-            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
-            this.dateTimePicker2.TabIndex = 2;
-            this.dateTimePicker2.Validated += new System.EventHandler(this.dateTimePicker2_Validated);
-            // 
-            // dateTimePicker3
-            // 
-            this.dateTimePicker3.Enabled = false;
-            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePicker3.Location = new System.Drawing.Point(71, 100);
-            this.dateTimePicker3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(265, 22);
-            this.dateTimePicker3.TabIndex = 3;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(20, 159);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(352, 57);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Calcola Differenza orari";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.dateTimePicker2);
-            this.groupBox1.Controls.Add(this.dateTimePicker3);
-            this.groupBox1.Location = new System.Drawing.Point(16, 15);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(401, 247);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Calcola differenae orarie";
-            // 
-            // dtExportStart
-            // 
-            this.dtExportStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtExportStart.Location = new System.Drawing.Point(224, 294);
-            this.dtExportStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dtExportStart.Name = "dtExportStart";
-            this.dtExportStart.Size = new System.Drawing.Size(163, 22);
-            this.dtExportStart.TabIndex = 6;
-            // 
-            // dtExportEnd
-            // 
-            this.dtExportEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtExportEnd.Location = new System.Drawing.Point(224, 326);
-            this.dtExportEnd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dtExportEnd.Name = "dtExportEnd";
-            this.dtExportEnd.Size = new System.Drawing.Size(163, 22);
-            this.dtExportEnd.TabIndex = 7;
-            // 
-            // chkEstero
-            // 
-            this.chkEstero.AutoSize = true;
-            this.chkEstero.Location = new System.Drawing.Point(36, 381);
-            this.chkEstero.Name = "chkEstero";
-            this.chkEstero.Size = new System.Drawing.Size(133, 21);
-            this.chkEstero.TabIndex = 8;
-            this.chkEstero.Text = "Trasferta Estera";
-            this.chkEstero.UseVisualStyleBackColor = true;
-            // 
-            // frmMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(418, 443);
-            this.Controls.Add(this.chkEstero);
-            this.Controls.Add(this.dtExportEnd);
-            this.Controls.Add(this.dtExportStart);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "frmMain";
-            this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-		}
-
-		#endregion
-
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.DateTimePicker dateTimePicker1;
-		private System.Windows.Forms.DateTimePicker dateTimePicker2;
-		private System.Windows.Forms.DateTimePicker dateTimePicker3;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.DateTimePicker dtExportStart;
-		private System.Windows.Forms.DateTimePicker dtExportEnd;
-        private System.Windows.Forms.CheckBox chkEstero;
+    /// <summary>
+    /// Pulire le risorse in uso.
+    /// </summary>
+    /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
     }
+
+    #region Codice generato da Progettazione Windows Form
+
+    /// <summary>
+    /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+    /// il contenuto del metodo con l'editor di codice.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      button1 = new System.Windows.Forms.Button();
+      dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+      dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+      dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+      button2 = new System.Windows.Forms.Button();
+      groupBox1 = new System.Windows.Forms.GroupBox();
+      dtExportStart = new System.Windows.Forms.DateTimePicker();
+      dtExportEnd = new System.Windows.Forms.DateTimePicker();
+      chkEstero = new System.Windows.Forms.CheckBox();
+      btnAddMese = new System.Windows.Forms.Button();
+      groupBox1.SuspendLayout();
+      SuspendLayout();
+      // 
+      // button1
+      // 
+      button1.Location = new System.Drawing.Point(36, 368);
+      button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      button1.Name = "button1";
+      button1.Size = new System.Drawing.Size(156, 71);
+      button1.TabIndex = 0;
+      button1.Text = "Esporta ore";
+      button1.UseVisualStyleBackColor = true;
+      button1.Click += button1_Click;
+      // 
+      // dateTimePicker1
+      // 
+      dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+      dateTimePicker1.Location = new System.Drawing.Point(71, 45);
+      dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      dateTimePicker1.Name = "dateTimePicker1";
+      dateTimePicker1.Size = new System.Drawing.Size(265, 27);
+      dateTimePicker1.TabIndex = 1;
+      dateTimePicker1.Validated += dateTimePicker1_Validated;
+      // 
+      // dateTimePicker2
+      // 
+      dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+      dateTimePicker2.Location = new System.Drawing.Point(71, 85);
+      dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      dateTimePicker2.Name = "dateTimePicker2";
+      dateTimePicker2.Size = new System.Drawing.Size(265, 27);
+      dateTimePicker2.TabIndex = 2;
+      dateTimePicker2.Validated += dateTimePicker2_Validated;
+      // 
+      // dateTimePicker3
+      // 
+      dateTimePicker3.Enabled = false;
+      dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+      dateTimePicker3.Location = new System.Drawing.Point(71, 125);
+      dateTimePicker3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      dateTimePicker3.Name = "dateTimePicker3";
+      dateTimePicker3.Size = new System.Drawing.Size(265, 27);
+      dateTimePicker3.TabIndex = 3;
+      // 
+      // button2
+      // 
+      button2.Location = new System.Drawing.Point(20, 199);
+      button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      button2.Name = "button2";
+      button2.Size = new System.Drawing.Size(352, 71);
+      button2.TabIndex = 4;
+      button2.Text = "Calcola Differenza orari";
+      button2.UseVisualStyleBackColor = true;
+      button2.Click += button2_Click;
+      // 
+      // groupBox1
+      // 
+      groupBox1.Controls.Add(dateTimePicker1);
+      groupBox1.Controls.Add(button2);
+      groupBox1.Controls.Add(dateTimePicker2);
+      groupBox1.Controls.Add(dateTimePicker3);
+      groupBox1.Location = new System.Drawing.Point(16, 19);
+      groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      groupBox1.Name = "groupBox1";
+      groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      groupBox1.Size = new System.Drawing.Size(401, 309);
+      groupBox1.TabIndex = 5;
+      groupBox1.TabStop = false;
+      groupBox1.Text = "Calcola differenae orarie";
+      // 
+      // dtExportStart
+      // 
+      dtExportStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+      dtExportStart.Location = new System.Drawing.Point(224, 368);
+      dtExportStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      dtExportStart.Name = "dtExportStart";
+      dtExportStart.Size = new System.Drawing.Size(163, 27);
+      dtExportStart.TabIndex = 6;
+      // 
+      // dtExportEnd
+      // 
+      dtExportEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+      dtExportEnd.Location = new System.Drawing.Point(224, 408);
+      dtExportEnd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      dtExportEnd.Name = "dtExportEnd";
+      dtExportEnd.Size = new System.Drawing.Size(163, 27);
+      dtExportEnd.TabIndex = 7;
+      // 
+      // chkEstero
+      // 
+      chkEstero.AutoSize = true;
+      chkEstero.Location = new System.Drawing.Point(36, 476);
+      chkEstero.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      chkEstero.Name = "chkEstero";
+      chkEstero.Size = new System.Drawing.Size(132, 24);
+      chkEstero.TabIndex = 8;
+      chkEstero.Text = "Trasferta Estera";
+      chkEstero.UseVisualStyleBackColor = true;
+      // 
+      // btnAddMese
+      // 
+      btnAddMese.Location = new System.Drawing.Point(288, 445);
+      btnAddMese.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      btnAddMese.Name = "btnAddMese";
+      btnAddMese.Size = new System.Drawing.Size(99, 31);
+      btnAddMese.TabIndex = 9;
+      btnAddMese.Text = "Mese successivo";
+      btnAddMese.UseVisualStyleBackColor = true;
+      btnAddMese.Click += btnAddMese_Click;
+      // 
+      // frmMain
+      // 
+      AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+      AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      ClientSize = new System.Drawing.Size(418, 554);
+      Controls.Add(btnAddMese);
+      Controls.Add(chkEstero);
+      Controls.Add(dtExportEnd);
+      Controls.Add(dtExportStart);
+      Controls.Add(groupBox1);
+      Controls.Add(button1);
+      Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      Name = "frmMain";
+      Text = "Form1";
+      groupBox1.ResumeLayout(false);
+      ResumeLayout(false);
+      PerformLayout();
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    private System.Windows.Forms.DateTimePicker dateTimePicker3;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.DateTimePicker dtExportStart;
+    private System.Windows.Forms.DateTimePicker dtExportEnd;
+    private System.Windows.Forms.CheckBox chkEstero;
+    private System.Windows.Forms.Button btnAddMese;
+  }
 }
 
--- FastCodeWriteOre/frmMain.cs
+++ FastCodeWriteOre/frmMain.cs
@@ -4,114 +4,126 @@ using System.Windows.Forms;
 
 namespace FastCodeWriteOre
 {
-    public partial class frmMain : Form
+  public partial class frmMain : Form
+  {
+    Impostazioni _impostazioni;
+
+
+    public frmMain()
     {
-        Impostazioni _impostazioni;
+      InitializeComponent();
+      _impostazioni = Impostazioni.Read();
+
+      if (_impostazioni.dataInizio < DateTime.Now.AddMonths(-3))
+        _impostazioni.dataInizio = DateTime.Now.AddMonths(-2);
+      if (_impostazioni.dataFine < DateTime.Now.AddMonths(-2))
+        _impostazioni.dataFine = DateTime.Now.AddMonths(-1);
+
+      dtExportStart.Value = _impostazioni.dataInizio;
+      dtExportEnd.Value = _impostazioni.dataFine;
 
 
-        public frmMain()
-        {
-            InitializeComponent();
-            _impostazioni = Impostazioni.Read();
-            dtExportStart.Value = _impostazioni.dataInizio;
-            dtExportEnd.Value = _impostazioni.dataFine;
-            dtExportStart.DataBindings.Add("Value", _impostazioni, "dataInizio");
-            dtExportEnd.DataBindings.Add("Value", _impostazioni, "dataFine");
-            chkEstero.DataBindings.Add("Checked", _impostazioni, "TrasfertaEstera");
-
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-            SelectFile();
-        }
-        private void SelectFile()
-        {
-            var domanda = (MessageBox.Show("Vuoi usare i file selezionati l'ultima volta?", "Domanda", 
-                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question));
-            if (domanda == DialogResult.Cancel)
-            {
-                return;
-            }
-            if (domanda == DialogResult.No)
-            {
-                _impostazioni.RaccoltaDati.File = "";
-                _impostazioni.DiarioCantiere.File = "";
-            }
-            _impostazioni.RaccoltaDati.File= "";
-            _impostazioni.TrasfertaEstera = chkEstero.Checked;
-
-            using (OpenFileDialog res = new OpenFileDialog())
-            {
-                if (string.IsNullOrEmpty(_impostazioni.DiarioCantiere.File))
-                {
-                    res.Title = "Seleziona file diario cantiere";
-                    //Filter
-                    res.Filter = "File excel|*fastcode*.xlsm;|Tutti i file|*.*";
-
-                    res.Multiselect = false;
-                    if (!string.IsNullOrEmpty(_impostazioni.DiarioCantiere.Cartella))
-                        res.InitialDirectory = _impostazioni.DiarioCantiere.Cartella;
-                    //When the user select the file
-                    if (res.ShowDialog() == DialogResult.OK)
-                    {
-                        _impostazioni.DiarioCantiere.File = res.FileName;
-                        _impostazioni.DiarioCantiere.Cartella = new FileInfo(_impostazioni.DiarioCantiere.File).DirectoryName;
-                    }
-                }
-                if (string.IsNullOrEmpty(_impostazioni.RaccoltaDati.File))
-                {
-                    res.Title = "Seleziona file RaccoltaDati x dip Cantieri";
-                    //Filter
-                    res.Filter = "File excel|*.xls;*.xlsx|Tutti i file|*.*";
-
-                    res.Multiselect = false;
-                    if (!string.IsNullOrEmpty(_impostazioni.RaccoltaDati.Cartella))
-                        res.InitialDirectory = _impostazioni.RaccoltaDati.Cartella;
-                    //When the user select the file
-                    if (res.ShowDialog() == DialogResult.OK)
-                    {
-                        _impostazioni.RaccoltaDati.File = res.FileName;
-                        _impostazioni.RaccoltaDati.Cartella = new FileInfo(_impostazioni.RaccoltaDati.File).DirectoryName;
-                    }
-                }
-                if (!string.IsNullOrEmpty(_impostazioni.RaccoltaDati.File)
-                    && !string.IsNullOrEmpty(_impostazioni.DiarioCantiere.File))
-                {
-                    var elab = new EsportaDatiClass(_impostazioni);
-                    elab.Elabora();
-                    Impostazioni.Save(_impostazioni);
-                }
-            }
-
-        }
-
-
-
-
-        private void dateTimePicker2_Validated(object sender, EventArgs e)
-        {
-            CalcolaDiff();
-        }
-
-        private void CalcolaDiff()
-        {
-            dateTimePicker3.Value = new DateTime(dateTimePicker2.Value.Date.Ticks + (dateTimePicker2.Value - dateTimePicker1.Value).Ticks);
-        }
-
-
-        private void dateTimePicker1_Validated(object sender, EventArgs e)
-        {
-            CalcolaDiff();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            CalcolaDiff();
-        }
+      dtExportStart.DataBindings.Add("Value", _impostazioni, "dataInizio");
+      dtExportEnd.DataBindings.Add("Value", _impostazioni, "dataFine");
+      chkEstero.DataBindings.Add("Checked", _impostazioni, "TrasfertaEstera");
 
 
     }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+      SelectFile();
+    }
+    private void SelectFile()
+    {
+      var domanda = (MessageBox.Show("Vuoi usare i file selezionati l'ultima volta?", "Domanda",
+          MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question));
+      if (domanda == DialogResult.Cancel)
+      {
+        return;
+      }
+      if (domanda == DialogResult.No)
+      {
+        _impostazioni.RaccoltaDati.File = "";
+        _impostazioni.DiarioCantiere.File = "";
+      }
+      _impostazioni.RaccoltaDati.File = "";
+      _impostazioni.TrasfertaEstera = chkEstero.Checked;
+
+      using (OpenFileDialog res = new OpenFileDialog())
+      {
+        if (string.IsNullOrEmpty(_impostazioni.DiarioCantiere.File))
+        {
+          res.Title = "Seleziona file diario cantiere";
+          //Filter
+          res.Filter = "File excel|*fastcode*.xlsm;|Tutti i file|*.*";
+
+          res.Multiselect = false;
+          if (!string.IsNullOrEmpty(_impostazioni.DiarioCantiere.Cartella))
+            res.InitialDirectory = _impostazioni.DiarioCantiere.Cartella;
+          //When the user select the file
+          if (res.ShowDialog() == DialogResult.OK)
+          {
+            _impostazioni.DiarioCantiere.File = res.FileName;
+            _impostazioni.DiarioCantiere.Cartella = new FileInfo(_impostazioni.DiarioCantiere.File).DirectoryName;
+          }
+        }
+        if (string.IsNullOrEmpty(_impostazioni.RaccoltaDati.File))
+        {
+          res.Title = "Seleziona file RaccoltaDati x dip Cantieri";
+          //Filter
+          res.Filter = "File excel|*.xls;*.xlsx|Tutti i file|*.*";
+
+          res.Multiselect = false;
+          if (!string.IsNullOrEmpty(_impostazioni.RaccoltaDati.Cartella))
+            res.InitialDirectory = _impostazioni.RaccoltaDati.Cartella;
+          //When the user select the file
+          if (res.ShowDialog() == DialogResult.OK)
+          {
+            _impostazioni.RaccoltaDati.File = res.FileName;
+            _impostazioni.RaccoltaDati.Cartella = new FileInfo(_impostazioni.RaccoltaDati.File).DirectoryName;
+          }
+        }
+        if (!string.IsNullOrEmpty(_impostazioni.RaccoltaDati.File)
+            && !string.IsNullOrEmpty(_impostazioni.DiarioCantiere.File))
+        {
+          var elab = new EsportaDatiClass(_impostazioni);
+          elab.Elabora();
+          Impostazioni.Save(_impostazioni);
+        }
+      }
+
+    }
+
+
+
+
+    private void dateTimePicker2_Validated(object sender, EventArgs e)
+    {
+      CalcolaDiff();
+    }
+
+    private void CalcolaDiff()
+    {
+      dateTimePicker3.Value = new DateTime(dateTimePicker2.Value.Date.Ticks + (dateTimePicker2.Value - dateTimePicker1.Value).Ticks);
+    }
+
+
+    private void dateTimePicker1_Validated(object sender, EventArgs e)
+    {
+      CalcolaDiff();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      CalcolaDiff();
+    }
+
+    private void btnAddMese_Click(object sender, EventArgs e)
+    {
+      dtExportStart.Value = dtExportEnd.Value.AddDays(1);
+      dtExportEnd.Value = dtExportStart.Value.AddDays(30);
+    }
+  }
 }
