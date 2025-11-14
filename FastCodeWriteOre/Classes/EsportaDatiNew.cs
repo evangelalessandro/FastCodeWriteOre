307726c2b0772649abaf59fcdc7fb658099e3c45
evangale <fastcodedevops@outlook.it>
2025-11-14 11:35:20 +0100

    fix prototipo template

--- FastCodeWriteOre/Classes/EsportaDatiNew.cs
+++ FastCodeWriteOre/Classes/EsportaDatiNew.cs
@@ -7,186 +7,176 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FastCodeWriteOre {
-	public class EsportaDatiClass {
-		Impostazioni _impostazioni;
-		public EsportaDatiClass(Impostazioni impostazioni) {
-			_impostazioni = impostazioni;
-		}
-		public void Elabora() {
+namespace FastCodeWriteOre
+{
+    public class EsportaDatiClass
+    {
+        private Impostazioni _impostazioni;
 
-			var dataOutput = new List<RowExcel>();
+        public EsportaDatiClass(Impostazioni impostazioni)
+        {
+            _impostazioni = impostazioni;
+        }
 
-			using (var excelRaccoltaDati = new ClosedXML.Excel.XLWorkbook(_impostazioni.RaccoltaDati.File)) {
-				using (var excelDiario = new ClosedXML.Excel.XLWorkbook(_impostazioni.DiarioCantiere.File)) {
+        public void Elabora()
+        {
+            var dataOutput = new List<RowExcel>();
 
+            using (var excelRaccoltaDati = new ClosedXML.Excel.XLWorkbook(_impostazioni.RaccoltaDati.File))
+            {
+                using (var excelDiario = new ClosedXML.Excel.XLWorkbook(_impostazioni.DiarioCantiere.File))
+                {
+                    var rowDiarioLast = excelDiario.Worksheet(1).Range("B:B").LastCellUsed().Address.RowNumber;
 
-					var rowDiarioLast = excelDiario.Worksheet(1).Range("B:B").LastCellUsed().Address.RowNumber;
+                    var list = new List<OreCantiere>();
+                    ClosedXML.Excel.IXLWorksheet meseSheet = excelRaccoltaDati.Worksheet(1);
 
-					var list = new List<OreCantiere>();
-					ClosedXML.Excel.IXLWorksheet meseSheet = excelRaccoltaDati.Worksheet(1);
+                    var listDate = DatePeriodo();
+                    EstraiDatiDiario(excelDiario, rowDiarioLast, list, listDate);
 
+                    var listGR = list.GroupBy(a => new { a.Commessa, a.Cantiere, a.Data }).ToList();
 
+                    var listOre = listGR.Select(a => new { a.Key.Cantiere, a.Key.Commessa, a.Key.Data, SommaOre = a.Sum(b => b.Ore) }).ToList();
 
-					var listDate = DatePeriodo();
-					EstraiDatiDiario(excelDiario, rowDiarioLast, list, listDate);
+                    dataOutput = listOre.Select(a => new RowExcel() { Data = a.Data, Commessa = a.Commessa, NumeroOre = a.SommaOre, Cantiere = a.Cantiere }).ToList();
 
-					var listGR = list.GroupBy(a => new { a.Commessa, a.Cantiere, a.Data }).ToList();
+                    var SommePerData = dataOutput.GroupBy(a => a.Data).Select(a => new { DataRif = a.Key, SommaOra = a.Sum(b => b.NumeroOre) }).Where(a => a.SommaOra != 8).ToList();
 
+                    foreach (var item in SommePerData)
+                    {
+                        var itemToUpdate = dataOutput.Where(a => a.Data == item.DataRif).OrderByDescending(a => a.NumeroOre).First();
+                        if (item.SommaOra > 8)
+                        {
+                            itemToUpdate.Note = $"Ore totali >8 in data {item.DataRif.ToString("dd/MM/yyyy")}; mettere {item.SommaOra - 8} di straordinari";
+                        }
+                        else if (item.SommaOra < 8)
+                        {
+                            itemToUpdate.Note = $"Solo {item.SommaOra} ore in data {item.DataRif.ToString("dd/MM/yyyy")}; mettere Rol nelle note";
+                        }
+                    }
 
+                    AggiungiTipoSede(dataOutput);
 
-					var listOre = listGR.Select(a => new { a.Key.Cantiere, a.Key.Commessa, a.Key.Data, SommaOre = a.Sum(b => b.Ore) }).ToList();
+                    RipulisciFoglio(meseSheet);
 
+                    ScriviOre(meseSheet, dataOutput);
 
-					dataOutput = listOre.Select(a => new RowExcel() { Data = a.Data, Commessa = a.Commessa, NumeroOre = a.SommaOre, Cantiere = a.Cantiere }).ToList();
+                    excelRaccoltaDati.Save();
+                }
+            }
 
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = _impostazioni.RaccoltaDati.File,
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(psi);
+            //MessageBox.Show("Operazione conclusa con successo", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-					var SommePerData = dataOutput.GroupBy(a => a.Data).Select(a => new { DataRif = a.Key, SommaOra = a.Sum(b => b.NumeroOre) }).Where(a => a.SommaOra != 8).ToList();
+            static void EstraiDatiDiario(ClosedXML.Excel.XLWorkbook excelDiario, int rowDiarioLast, List<OreCantiere> list, List<DateTime> listDate)
+            {
+                for (int i = 6; i <= rowDiarioLast; i++)
+                {
+                    var dataDiario = DateTime.Parse(excelDiario.Worksheet(1).Cell(i, 2).Value.ToString()).Date;
+                    if (listDate.Contains(dataDiario))
+                    {
+                        var diarioItem =
+                        (new OreCantiere()
+                        {
+                            Commessa = excelDiario.Worksheet(1).Cell(i, 3).Value.ToString().Trim(),
+                            Data = dataDiario,
+                            Ore = decimal.Parse(excelDiario.Worksheet(1).Cell(i, 7).Value.ToString()),
+                            Cantiere = excelDiario.Worksheet(1).Cell(i, 5).Value.ToString() == "C",
+                        });
+                        list.Add(diarioItem);
+                    }
+                }
+            }
 
-					foreach (var item in SommePerData) {
-						var itemToUpdate = dataOutput.Where(a => a.Data == item.DataRif).OrderByDescending(a => a.NumeroOre).First();
-						if (item.SommaOra > 8) {
+            void AggiungiTipoSede(List<RowExcel> dataOutput)
+            {
+                foreach (var itemOut in dataOutput.Where(a => a.Cantiere || _impostazioni.TrasfertaEstera || !string.IsNullOrEmpty(a.Commessa)).ToList())
+                {
+                    string opt = "";
 
+                    if (string.IsNullOrEmpty(itemOut.Commessa) && _impostazioni.TrasfertaEstera)
+                    {
+                        opt = "Giorno non lavorativo cantiere Estero";
+                    }
+                    else if (itemOut.Cantiere)
+                    {
+                        if (_impostazioni.TrasfertaEstera)
+                        {
+                            opt = "CANTIERE EUROPA";
+                        }
+                        else
+                        {
+                            opt = "CANTIERE ITALIA";
+                        }
+                    }
+                    else if (!string.IsNullOrEmpty(itemOut.Commessa))
+                    {
+                        opt = "SEDE";
+                    }
+                    itemOut.SedeLavoro = opt;
+                }
+            }
+        }
 
-							itemToUpdate.Note = $"Ore totali >8 in data {item.DataRif.ToString("dd/MM/yyyy")}; mettere {item.SommaOra - 8} di straordinari";
+        private List<DateTime> DatePeriodo()
+        {
+            return Enumerable
+            .Range(0, int.MaxValue)
+            .Select(index => new DateTime?(_impostazioni.dataInizio.AddDays(index)))
+            .TakeWhile(date => date <= _impostazioni.dataFine)
+            .Select(a => a.Value.Date)
+            .ToList();
+        }
 
-						} else if (item.SommaOra < 8) {
-							itemToUpdate.Note = $"Solo {item.SommaOra} ore in data {item.DataRif.ToString("dd/MM/yyyy")}; mettere Rol nelle note";
-						}
-					}
+        private void RipulisciFoglio(ClosedXML.Excel.IXLWorksheet meseSheet)
+        {
+            meseSheet.Range("A2:Z100").Clear(ClosedXML.Excel.XLClearOptions.Contents);
+        }
 
-					/*aggiungo le date mancanti del periodo selezionato*/
-					//foreach (var itemData in listDate) {
-					//  if (dataOutput.Where(a => a.Data == itemData).Count() == 0) {
-					//    var itemRow = (new RowExcel() { Data = itemData });
+        private void ScriviOre(
+            ClosedXML.Excel.IXLWorksheet meseSheet, List<RowExcel> dataOutput)
+        {
+            int row = 2;
 
+            while (dataOutput.Count() > 0)
+            {
+                var itemRowOutput = dataOutput.Select(a => a).OrderBy(a => a.Data).FirstOrDefault();
+                dataOutput.Remove(itemRowOutput);
 
-					//    dataOutput.Add(itemRow);
-					//  }
-					//}
-					AggiungiTipoSede(dataOutput);
+                meseSheet.Cell(row, 1).Value = itemRowOutput.Data.ToString("dd/MM/yyyy");
+                meseSheet.Cell(row, 2).Value = System.Globalization.ISOWeek.GetWeekOfYear(itemRowOutput.Data);
+                meseSheet.Cell(row, 3).Value = "Alessandro Evangelisti";
+                meseSheet.Cell(row, 4).Value = "DEV SW PC";
+                meseSheet.Cell(row, 5).Value = itemRowOutput.SedeLavoro;
+                meseSheet.Cell(row, 7).Value = itemRowOutput.Commessa.Split(" ").Where(a => a.Length > 0).Last();
+                meseSheet.Cell(row, 8).Value = itemRowOutput.Commessa;
 
-					RipulisciFoglio(meseSheet);
+                meseSheet.Cell(row, 10).Value = itemRowOutput.NumeroOre;
 
+                meseSheet.Cell(row, 17).Value = itemRowOutput.Note;
 
-					ScriviOre(meseSheet, dataOutput);
+                row++;
+            }
+        }
 
-					excelRaccoltaDati.Save();
-				}
+        private static void Pernotto(ClosedXML.Excel.IXLWorksheet meseSheet, int row)
+        {
+            meseSheet.Cell(row, 13).Value = "x";
+            meseSheet.Cell(row, 14).Value = "x";
+            meseSheet.Cell(row, 15).Value = "x";
+        }
 
-			}
-
-
-			var psi = new System.Diagnostics.ProcessStartInfo {
-				FileName = _impostazioni.RaccoltaDati.File,
-				UseShellExecute = true
-			};
-			System.Diagnostics.Process.Start(psi);
-			//MessageBox.Show("Operazione conclusa con successo", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-			static void EstraiDatiDiario(ClosedXML.Excel.XLWorkbook excelDiario, int rowDiarioLast, List<OreCantiere> list, List<DateTime> listDate) {
-				for (int i = 6; i <= rowDiarioLast; i++) {
-					var dataDiario = DateTime.Parse(excelDiario.Worksheet(1).Cell(i, 2).Value.ToString()).Date;
-					if (listDate.Contains(dataDiario)) {
-						var diarioItem =
-						(new OreCantiere() {
-							Commessa = excelDiario.Worksheet(1).Cell(i, 3).Value.ToString(),
-							Data = dataDiario,
-							Ore = decimal.Parse(excelDiario.Worksheet(1).Cell(i, 7).Value.ToString()),
-							Cantiere = excelDiario.Worksheet(1).Cell(i, 5).Value.ToString() == "C",
-
-						});
-						list.Add(diarioItem);
-					}
-				}
-			}
-
-			void AggiungiTipoSede(List<RowExcel> dataOutput) {
-				foreach (var itemOut in dataOutput.Where(a => a.Cantiere || _impostazioni.TrasfertaEstera || !string.IsNullOrEmpty(a.Commessa)).ToList()) {
-					string opt = "";
-
-					if (string.IsNullOrEmpty(itemOut.Commessa) && _impostazioni.TrasfertaEstera) {
-
-						opt = "Giorno non lavorativo cantiere Estero";
-					} else if (itemOut.Cantiere) {
-
-						if (_impostazioni.TrasfertaEstera) {
-							opt = "CANTIERE EUROPA";
-						} else {
-							opt = "CANTIERE ITALIA";
-						}
-					} else if (!string.IsNullOrEmpty(itemOut.Commessa)) {
-						opt = "SEDE";
-
-					}
-					itemOut.SedeLavoro = opt;
-				}
-			}
-		}
-
-		private List<DateTime> DatePeriodo() {
-			return Enumerable
-			.Range(0, int.MaxValue)
-			.Select(index => new DateTime?(_impostazioni.dataInizio.AddDays(index)))
-			.TakeWhile(date => date <= _impostazioni.dataFine)
-			.Select(a => a.Value.Date)
-			.ToList();
-
-		}
-
-		private void RipulisciFoglio(ClosedXML.Excel.IXLWorksheet meseSheet) {
-
-			meseSheet.Range("A2:Z100").Clear(ClosedXML.Excel.XLClearOptions.Contents);
-
-		}
-
-
-		private void ScriviOre(
-			ClosedXML.Excel.IXLWorksheet meseSheet, List<RowExcel> dataOutput) {
-			int row = 2;
-
-			var it = new CultureInfo("it-IT");
-			while (dataOutput.Count() > 0) {
-
-				var itemRowOutput = dataOutput.Select(a => a).OrderBy(a => a.Data).FirstOrDefault();
-				dataOutput.Remove(itemRowOutput);
-
-
-				meseSheet.Cell(row, 1).Value = itemRowOutput.Data.ToString("dd/MM/yyyy");
-				meseSheet.Cell(row, 2).Value = System.Globalization.ISOWeek.GetWeekOfYear(itemRowOutput.Data);
-				meseSheet.Cell(row, 3).Value = "Alessandro Evangelisti";
-				meseSheet.Cell(row, 4).Value = "DEV SW PC";
-				meseSheet.Cell(row, 5).Value = itemRowOutput.SedeLavoro;
-				meseSheet.Cell(row, 7).Value = itemRowOutput.Commessa.Split(" ").Last();
-				meseSheet.Cell(row, 8).Value = itemRowOutput.Commessa;
-
-
-
-				meseSheet.Cell(row, 10).Value = itemRowOutput.NumeroOre;
-
-				meseSheet.Cell(row, 17).Value = itemRowOutput.Note;
-
-
-
-				row++;
-			}
-
-
-		}
-
-
-
-		private static void Pernotto(ClosedXML.Excel.IXLWorksheet meseSheet, int row) {
-			meseSheet.Cell(row, 13).Value = "x";
-			meseSheet.Cell(row, 14).Value = "x";
-			meseSheet.Cell(row, 15).Value = "x";
-		}
-
-		internal class OreCantiere {
-			public DateTime Data { get; set; }
-			public string Commessa { get; set; }
-			public decimal Ore { get; set; }
-			public bool Cantiere { get; set; }
-		}
-	}
+        internal class OreCantiere
+        {
+            public DateTime Data { get; set; }
+            public string Commessa { get; set; }
+            public decimal Ore { get; set; }
+            public bool Cantiere { get; set; }
+        }
+    }
 }
--- FastCodeWriteOre/frmMain.cs
+++ FastCodeWriteOre/frmMain.cs
@@ -1,129 +1,142 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
 
-namespace FastCodeWriteOre {
-  public partial class frmMain : Form {
-    Impostazioni _impostazioni;
+namespace FastCodeWriteOre
+{
+    public partial class frmMain : Form
+    {
+        Impostazioni _impostazioni;
 
 
-    public frmMain() {
-      InitializeComponent();
-      _impostazioni = Impostazioni.Read();
-      this.Text = "Esporta ore " + Assembly.GetExecutingAssembly().GetName().Version;
+        public frmMain()
+        {
+            InitializeComponent();
+            _impostazioni = Impostazioni.Read();
+            this.Text = "Esporta ore " + Assembly.GetExecutingAssembly().GetName().Version;
 
-      if (_impostazioni.dataInizio < DateTime.Now.AddMonths(-3))
-        _impostazioni.dataInizio = DateTime.Now.AddMonths(-2);
-      if (_impostazioni.dataFine < DateTime.Now.AddMonths(-2))
-        _impostazioni.dataFine = DateTime.Now.AddMonths(-1);
+            if (_impostazioni.dataInizio < DateTime.Now.AddMonths(-3))
+                _impostazioni.dataInizio = DateTime.Now.AddMonths(-2);
+            if (_impostazioni.dataFine < DateTime.Now.AddMonths(-2))
+                _impostazioni.dataFine = DateTime.Now.AddMonths(-1);
 
-      dtExportStart.Value = _impostazioni.dataInizio;
-      dtExportEnd.Value = _impostazioni.dataFine;
-
-
-      dtExportStart.DataBindings.Add("Value", _impostazioni, "dataInizio");
-      dtExportEnd.DataBindings.Add("Value", _impostazioni, "dataFine");
-      chkEstero.DataBindings.Add("Checked", _impostazioni, "TrasfertaEstera");
+            dtExportStart.Value = _impostazioni.dataInizio;
+            dtExportEnd.Value = _impostazioni.dataFine;
 
 
-    }
+            dtExportStart.DataBindings.Add("Value", _impostazioni, "dataInizio");
+            dtExportEnd.DataBindings.Add("Value", _impostazioni, "dataFine");
+            chkEstero.DataBindings.Add("Checked", _impostazioni, "TrasfertaEstera");
 
-    private void button1_Click(object sender, EventArgs e) {
 
-      SelectFile();
-    }
-    private void SelectFile() {
-      if (_impostazioni.dataInizio >= _impostazioni.dataFine) {
-        MessageBox.Show("Data inizio maggiore di quella di fine!!", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        return;
-      }
-
-      var domanda = (MessageBox.Show("Vuoi usare i file selezionati l'ultima volta?", "Domanda",
-          MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question));
-      if (domanda == DialogResult.Cancel) {
-        return;
-      }
-      if (domanda == DialogResult.No) {
-        _impostazioni.RaccoltaDati.File = "";
-        _impostazioni.DiarioCantiere.File = "";
-      }
-      _impostazioni.RaccoltaDati.File = "";
-      _impostazioni.TrasfertaEstera = chkEstero.Checked;
-
-      using (OpenFileDialog res = new OpenFileDialog()) {
-        if (string.IsNullOrEmpty(_impostazioni.DiarioCantiere.File)) {
-          res.Title = "Seleziona file diario cantiere";
-          //Filter
-          res.Filter = "File excel|*fastcode*.xlsm;|Tutti i file|*.*";
-
-          res.Multiselect = false;
-          if (!string.IsNullOrEmpty(_impostazioni.DiarioCantiere.Cartella))
-            res.InitialDirectory = _impostazioni.DiarioCantiere.Cartella;
-          //When the user select the file
-          if (res.ShowDialog() == DialogResult.OK) {
-            _impostazioni.DiarioCantiere.File = res.FileName;
-            _impostazioni.DiarioCantiere.Cartella = new FileInfo(_impostazioni.DiarioCantiere.File).DirectoryName;
-          }
         }
-        if (string.IsNullOrEmpty(_impostazioni.RaccoltaDati.File)) {
-          res.Title = "Seleziona file RaccoltaDati x dip Cantieri";
-          //Filter
-          res.Filter = "File excel|*.xls;*.xlsx|Tutti i file|*.*";
 
-          res.Multiselect = false;
-          if (!string.IsNullOrEmpty(_impostazioni.RaccoltaDati.Cartella))
-            res.InitialDirectory = _impostazioni.RaccoltaDati.Cartella;
-          //When the user select the file
-          if (res.ShowDialog() == DialogResult.OK) {
-            _impostazioni.RaccoltaDati.File = res.FileName;
-            _impostazioni.RaccoltaDati.Cartella = new FileInfo(_impostazioni.RaccoltaDati.File).DirectoryName;
-          }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SelectFile();
         }
-        if (!string.IsNullOrEmpty(_impostazioni.RaccoltaDati.File)
-            && !string.IsNullOrEmpty(_impostazioni.DiarioCantiere.File)) {
-          var elab = new EsportaDatiClass(_impostazioni);
-          elab.Elabora();
-          Impostazioni.Save(_impostazioni);
+        private void SelectFile()
+        {
+            if (_impostazioni.dataInizio >= _impostazioni.dataFine)
+            {
+                MessageBox.Show("Data inizio maggiore di quella di fine!!", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var domanda = (MessageBox.Show("Vuoi usare i file selezionati l'ultima volta?", "Domanda",
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question));
+            if (domanda == DialogResult.Cancel)
+            {
+                return;
+            }
+            if (domanda == DialogResult.No)
+            {
+                _impostazioni.RaccoltaDati.File = "";
+                _impostazioni.DiarioCantiere.File = "";
+            }
+            _impostazioni.RaccoltaDati.File = "";
+            _impostazioni.TrasfertaEstera = chkEstero.Checked;
+
+            using (OpenFileDialog res = new OpenFileDialog())
+            {
+                if (string.IsNullOrEmpty(_impostazioni.DiarioCantiere.File))
+                {
+                    res.Title = "Seleziona file diario cantiere";
+                    //Filter
+                    res.Filter = "File excel|*fastcode*.xlsm;|Tutti i file|*.*";
+
+                    res.Multiselect = false;
+                    if (!string.IsNullOrEmpty(_impostazioni.DiarioCantiere.Cartella))
+                        res.InitialDirectory = _impostazioni.DiarioCantiere.Cartella;
+                    //When the user select the file
+                    if (res.ShowDialog() == DialogResult.OK)
+                    {
+                        _impostazioni.DiarioCantiere.File = res.FileName;
+                        _impostazioni.DiarioCantiere.Cartella = new FileInfo(_impostazioni.DiarioCantiere.File).DirectoryName;
+                    }
+                }
+                var dirTemp = SpecialDirectories.Temp;
+                var tempFile = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName + "\\template\\Prototipo.xlsx";
+                var destFile = dirTemp + "\\Prototipo.xlsx";
+                System.IO.File.Copy(tempFile, destFile, true);
+
+                _impostazioni.RaccoltaDati.File = destFile;
+                _impostazioni.RaccoltaDati.Cartella = new FileInfo(_impostazioni.RaccoltaDati.File).DirectoryName;
+
+
+                if (!string.IsNullOrEmpty(_impostazioni.RaccoltaDati.File)
+                    && !string.IsNullOrEmpty(_impostazioni.DiarioCantiere.File))
+                {
+                    var elab = new EsportaDatiClass(_impostazioni);
+                    elab.Elabora();
+                    Impostazioni.Save(_impostazioni);
+                }
+            }
+
         }
-      }
 
+
+
+
+        private void dateTimePicker2_Validated(object sender, EventArgs e)
+        {
+            CalcolaDiff();
+        }
+
+        private void CalcolaDiff()
+        {
+            dateTimePicker3.Value = new DateTime(dateTimePicker2.Value.Date.Ticks + (dateTimePicker2.Value - dateTimePicker1.Value).Ticks);
+        }
+
+
+        private void dateTimePicker1_Validated(object sender, EventArgs e)
+        {
+            CalcolaDiff();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CalcolaDiff();
+        }
+
+        private void btnAddMese_Click(object sender, EventArgs e)
+        {
+            btnAddMese.Focus();
+
+
+            var dt = dtExportEnd.Value.AddDays(1);
+            _impostazioni.dataInizio = dt;
+            _impostazioni.dataFine = dt.AddDays(30);
+            dtExportStart.Value = _impostazioni.dataInizio;
+            dtExportEnd.Value = _impostazioni.dataFine;
+            dtExportStart.Refresh();
+            dtExportEnd.Refresh();
+
+            //dtExportStart.DataBindings.Add("Value", _impostazioni, "dataInizio");
+            //dtExportEnd.DataBindings.Add("Value", _impostazioni, "dataFine");
+        }
     }
-
-
-
-
-    private void dateTimePicker2_Validated(object sender, EventArgs e) {
-      CalcolaDiff();
-    }
-
-    private void CalcolaDiff() {
-      dateTimePicker3.Value = new DateTime(dateTimePicker2.Value.Date.Ticks + (dateTimePicker2.Value - dateTimePicker1.Value).Ticks);
-    }
-
-
-    private void dateTimePicker1_Validated(object sender, EventArgs e) {
-      CalcolaDiff();
-    }
-
-    private void button2_Click(object sender, EventArgs e) {
-      CalcolaDiff();
-    }
-
-    private void btnAddMese_Click(object sender, EventArgs e) {
-      btnAddMese.Focus();
-
-
-      var dt = dtExportEnd.Value.AddDays(1);
-      _impostazioni.dataInizio = dt;
-      _impostazioni.dataFine = dt.AddDays(30);
-      dtExportStart.Value = _impostazioni.dataInizio;
-      dtExportEnd.Value = _impostazioni.dataFine;
-      dtExportStart.Refresh();
-      dtExportEnd.Refresh();
-
-      //dtExportStart.DataBindings.Add("Value", _impostazioni, "dataInizio");
-      //dtExportEnd.DataBindings.Add("Value", _impostazioni, "dataFine");
-    }
-  }
 }
